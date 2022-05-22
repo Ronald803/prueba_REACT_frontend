b733003b24251b8dc4d204b3b29fd0fe5f8d6a70
ronaldvic <ronaldblancobalboa@gmail.com>
2022-05-22 17:53:41 -0400

    NavBar created

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes} from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import './App.css';
 import Navigation from './components/Navigation';
 import Home from './components/Home';
