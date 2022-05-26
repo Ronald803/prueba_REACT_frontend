c0256e1c6864d2b90b1c60141133f30d288b90cd
ronaldvic <ronaldblancobalboa@gmail.com>
2022-05-25 23:52:39 -0400

    nav bar created

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,8 @@
  export default class Home extends Component {
    render() {
      return (
-       <div>Home</div>
+       <div>Home
+       </div>
      )
    }
  }
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,33 +4,33 @@ import {Link} from 'react-router-dom'
 export default class Navigation extends Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-lg bg-light">
-        <div className="container-fluid">
-          <Link className='navbar-brand' to="/">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container-fluid p-2">
+          <Link className='navbar-brand ' to="/">
               Eventos Gran Poder
           </Link>
 
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav ms-auto">
-              <li class="nav-item">
-                <Link className="nav-link" to="/">Inicio</Link>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item">
+                <Link className="nav-link active" aria-current="page" to="/">Inicio</Link>
               </li>
-              <li class="nav-item">
+              <li className="nav-item">
                 <Link className="nav-link" to="/servicios/salones">Salones</Link>
               </li>
-              <li class="nav-item">
+              <li className="nav-item">
                 <Link className="nav-link" to="/servicios/comida">Comida</Link>
               </li>
-              <li class="nav-item">
+              <li className="nav-item">
                 <Link className="nav-link" to="/servicios/musica">Música</Link>
               </li>
-              <li class="nav-item">
+              <li className="nav-item">
                 <Link className="nav-link" to="/servicios/musica">Bartender</Link>
               </li>
-              <li class="nav-item">
+              <li className="nav-item">
                 <Link className="nav-link" to="/usuarios">Iniciar Sesión</Link>
               </li>
 
