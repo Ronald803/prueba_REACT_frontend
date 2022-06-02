af714fec1d267439f2e91ea7cb87aff31a37a267
ronaldvic <ronaldblancobalboa@gmail.com>
2022-06-02 18:11:19 -0400

    home page created

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -32,6 +32,58 @@ export class Home extends Component {
             <span className="visually-hidden">Next</span>
           </button>
         </div>
+        
+        <section className="w-50 mx-auto text-center pt-5" id="intro">
+          <h1 className="p-3 fs-2 border-top border-3">Contamos con todo lo que necesites para ese<span className="text-primary"> día especial</span></h1>
+          <p className="p-3 fs-4"><span className="text-primary">Eventos Gran Poder</span> te ayuda a organizar un evento inolvidable. Salones, música, decoración, catering, bartenders, y más...</p>
+        </section>
+
+        <section className="container-fluid">
+            <div className="row w-75 mx-auto my-5 servicio-fila">
+              <div className="col-lg-6 col-md-12 col-sm-12 d-flex  my-5 icono-wrap">
+                  <img src="./img/calidad.webp" alt="calidad" width="180" heigth="160"/>
+                  <div>
+                    <h3 className="fs-5 mt-4 px-4 pb-1">Servicio 5 estrellas</h3>
+                    <p className="px-4">Salones con alto estandar de comodidad y calidad</p>
+                  </div>
+              </div>
+
+              <div className="col-lg-6 col-md-12 col-sm-12 d-flex my-5 icono-wrap">
+                  <img src="./img/calidad.webp" alt="calidad" width="180" heigth="160"/>
+                  <div>
+                    <h3 className="fs-5 mt-4 px-4 pb-1">Servicio 5 estrellas</h3>
+                    <p className="px-4">Salones con alto estandar de comodidad y calidad</p>
+                  </div>
+              </div>
+                 
+              <div className="col-lg-6 col-md-12 col-sm-12 d-flex my-5 icono-wrap">
+                  <img src="./img/calidad.webp" alt="calidad" width="180" heigth="160"/>
+                  <div>
+                    <h3 className="fs-5 mt-4 px-4 pb-1">Servicio 5 estrellas</h3>
+                    <p className="px-4">Salones con alto estandar de comodidad y calidad</p>
+                  </div>
+              </div>
+
+              <div className="col-lg-6 col-md-12 col-sm-12 d-flex my-5 icono-wrap">
+                  <img src="./img/calidad.webp" alt="calidad" width="180" heigth="160"/>
+                  <div>
+                    <h3 className="fs-5 mt-4 px-4 pb-1">Servicio 5 estrellas</h3>
+                    <p className="px-4">Salones con alto estandar de comodidad y calidad</p>
+                  </div>
+              </div>   
+            </div>
+        </section>
+
+        <section>
+          <div  className='w-75 m-auto text-center' id="equipo">
+              <h1 className='mb-5 fs-2'>Tenemos todo para el evento de tus <span className='text-primary'>sueños</span></h1>
+              <p className='fs-4'>El lugar, la música, la atención para tus invitados, decoración, la comida, y más detalles que puedes elegir de nuestro catálogo donde te ofrecemos opciones para ese día especial. </p>
+          </div>
+          <div className='mt-5 text-center'>
+            <img className='img-fluid' src="./img/contactos.jpg" alt="contactos"/>
+          </div>
+        </section>
+
       </div>
 
     )
